fc27f277972c25569ddae984a8c30acba9581f86
Mckade Daines <mckadedaines1@gmail.com>
2022-12-26 22:48:48 -0700

    banner done. Moving onto the panels next.

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
+          alt="logo"
+        />
+
+        <img
+          className="nav__avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/bb3a8833850498.56ba69ac33f26.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzY-VaMdpBdTOfdIJT3dDAR0twux9X0FM",
+    authDomain: "netflix-clone-6d322.firebaseapp.com",
+    projectId: "netflix-clone-6d322",
+    storageBucket: "netflix-clone-6d322.appspot.com",
+    messagingSenderId: "699507504786",
+    appId: "1:699507504786:web:88db8b9f436ff05c28184b"
+  };
